104d5bd00b28ebeb660c2eab06e148c387080e45
Egor <Golubtcov01@gmail.com>
2023-05-21 21:39:04 +0300

    ПРАКТИЧЕСКАЯ 2

---
+++ Zadanie1/Zadanie1/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+           
+                int M = int.Parse(TbNumberA.Text);
+                int ton = M / 1000;
+
+                TextBlockAnswer.Text = $"Количество полных тонн:{ton}";
+           
+        }
+    }
+}
---
+++ Zadanie4Dop/Zadanie4Dop/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zadanie4Dop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            int A = int.Parse(TbNumberA.Text);
+            int B = int.Parse(TbNumberB.Text);
+            int C = int.Parse(TbNumberC.Text);
+
+
+
+            int numSquares = (A / C) * (B / C);
+            int freeArea = A * B - numSquares * C * C;
+
+            TextBlockAnswer.Text = $"Количество квадратов:{ numSquares} \n Площадь незанятой части:{freeArea} ";
+        }
+    }
+}
